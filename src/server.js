748405114fc448fb0e43825eb737f5748d6be4c9
raimundoneto01 <raimundogomes826@gmail.com>
2023-07-10 21:45:39 -0300

    Conectando ao Banco com .ENV

--- src/server.js
+++ src/server.js
@@ -1,14 +1,32 @@
 //  criando primeira aplicação e subindo servidor:
 // acessar: https://expressjs.com/pt-br/starter/hello-world.html tem o passo a passo. 
 
-const express = require('express')
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+
 const app = express()
 const host = 'localhost'
 const port = 5000
 
+//endpoint inicial
 app.get('/', (req, res) => {
-    res.send('Oi mundo')
+    res.send('Servidor rodando')
 })
-app.listen(port, () => {
-    console.log(`Estou ouvindo na porta http://${host}:${port}`)
-  });
+
+//Criando constantes pra acessar o login do banco de dados
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+
+//conectando no banco mongo atlas
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.aybtzfg.mongodb.net/usuario?retryWrites=true&w=majority`)
+.then(()=> {
+console.log('BD CONECTADO');
+})
+.catch(error => {
+    console.log('erro ao conectar', error);
+})
+ app.listen(port, () => {
+     console.log(`Estou ouvindo na porta http://${host}:${port}`)
+   });
